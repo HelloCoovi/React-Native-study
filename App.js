78830d9e10139a0dbfefa3ae117c8cb98a23f92b
jangg0627 <jangg06270@gmail.com>
2023-02-10 17:56:10 +0900

    💄 레이아웃 생성(flex + style) flex box와 기타 style을 사용해서 레이아웃 생성

--- App.js
+++ App.js
@@ -4,34 +4,15 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      {/* <View style={styles.inputContainer}>
+      <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="이곳에 나의 목표를 적으세요!"
         />
         <Button title="목표 만들기" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>나의 목표들...</Text>
-      </View> */}
-
-      <View
-        style={{
-          width: "100%",
-          height: "98%",
-          borderWidth: 3,
-          borderColor: "black",
-        }}
-      >
-        <View style={{ flex: 1, backgroundColor: "#AEE2FF" }}>
-          <Text style={styles.textStyle}>box1</Text>
-        </View>
-        <View style={{ flex: 1, backgroundColor: "#F48484" }}>
-          <Text style={styles.textStyle}>box2</Text>
-        </View>
-        <View style={{ flex: 1, backgroundColor: "#F9F54B" }}>
-          <Text style={styles.textStyle}>box3</Text>
-        </View>
       </View>
     </View>
   );
@@ -39,23 +20,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    paddingTop: 40,
-    paddingLeft: 5,
-    paddingRight: 5,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
-  // inputContainer: {
-  //   flexDirection: "row",
-  //   justifyContent: "space-between",
-  // },
-  // textInput: {
-  //   borderWidth: 1,
-  //   borderColor: "#cccccc",
-  //   width: "80%",
-  //   marginRight: 8,
-  //   padding: 8,
-  // },
-  textStyle: {
-    fontSize: 50,
-    fontWeight: "bold",
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
