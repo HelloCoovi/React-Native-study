862d4a25f4bf7cdf868db08258b68f8ab1fe70a2
jangg0627 <jangg06270@gmail.com>
2023-02-10 17:25:52 +0900

    💄 flexbox 실습 간단한 flex박스 예제를 만들어봄

--- App.js
+++ App.js
@@ -1,38 +1,61 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text
-          style={{
-            margin: 16,
-            padding: 16,
-            borderWidth: 2,
-            borderColor: "red",
-          }}
-        >
-          This is Inline styling
-        </Text>
+    <View style={styles.appContainer}>
+      {/* <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이곳에 나의 목표를 적으세요!"
+        />
+        <Button title="목표 만들기" />
       </View>
-      <Text style={styles.myText}>This is StyleSheet styling</Text>
-      <Button title="눌러주세요!"></Button>
+      <View>
+        <Text>나의 목표들...</Text>
+      </View> */}
+
+      <View
+        style={{
+          width: "100%",
+          height: "98%",
+          borderWidth: 3,
+          borderColor: "black",
+        }}
+      >
+        <View style={{ flex: 1, backgroundColor: "#AEE2FF" }}>
+          <Text style={styles.textStyle}>box1</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "#F48484" }}>
+          <Text style={styles.textStyle}>box2</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "#F9F54B" }}>
+          <Text style={styles.textStyle}>box3</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    paddingTop: 40,
+    paddingLeft: 5,
+    paddingRight: 5,
   },
-  myText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "blue",
+  // inputContainer: {
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  // },
+  // textInput: {
+  //   borderWidth: 1,
+  //   borderColor: "#cccccc",
+  //   width: "80%",
+  //   marginRight: 8,
+  //   padding: 8,
+  // },
+  textStyle: {
+    fontSize: 50,
+    fontWeight: "bold",
   },
 });
