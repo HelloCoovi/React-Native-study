011553ac1d81a7251f8c65b92fed7856c91a0f79
jangg0627 <jangg06270@gmail.com>
2023-02-10 10:52:43 +0900

    ✨ core component 사용하기 View, Text, Button 등의 핵심 컴포넌트를 사용해봤음

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hello, world!</Text>
+      </View>
+      <Text>리액트 네이티브 시작합니다!</Text>
+      <Button title="눌러주세요!"></Button>
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
