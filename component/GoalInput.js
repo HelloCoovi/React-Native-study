b39ea38f749520dce8ee5a75919e7e72590fb9de
jangg0627 <jangg06270@gmail.com>
2023-02-14 10:38:39 +0900

    💄 모달 UI 수정 + "취소" 버튼 추가 각종 style 추가

--- component/GoalInput.js
+++ component/GoalInput.js
@@ -21,7 +21,14 @@ function GoalInput(props) {
           placeholder="이곳에 나의 목표를 적으세요!"
           value={enteredGoalText}
         />
-        <Button onPress={addGoalHandler} title="목표 만들기" />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button onPress={addGoalHandler} title="목표 만들기" />
+          </View>
+          <View style={styles.button}>
+            <Button title="취소하기" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -31,18 +38,25 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 });
 
