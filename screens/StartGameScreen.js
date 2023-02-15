c7a4c9ee1ee8e6ef4987b49d437ca3b0a8e240f0
jangg0627 <jangg06270@gmail.com>
2023-02-15 14:50:53 +0900

    💄 기본적인 input 창 UI 수정 android, ios가 서로 다른 스타일링 요소가 사용된다는 점을 염두하며 스타일링함 그림자 옵션은 두 os에 따라 다르기 때문에 따로 처리해야한다.  - android:        elevation 옵션을 넣어주면 간단히 해결 - ios:        shadow 옵션을 활용해서 그림자를 생성한다.          - shadowColor: 그림자 색상          - shadowOffset: 그림자의 크기          - shadowRadius: 그림자 테두리          - shadowOpacity: 그림자의 진함 정도

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,30 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
-      <PrimaryButton>Rest</PrimaryButton>
+      <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: "#72063c",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
