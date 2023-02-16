23ecbceba366006de5ac78c886ef6ac1e191a969
jangg0627 <jangg0627@naver.com>
2023-02-16 17:20:20 +0900

    💄 GameScreen 설계

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 function GameScreen() {
-  return <Text>This is GameScreen!</Text>;
+  return (
+    <View>
+      <Text>상대방의 예상값</Text>
+      {/* 예상값 */}
+      <Text>높나요? 낮나요?</Text>
+      {/* + 버튼 */}
+      {/* - 버튼 */}
+      {/*  라운드 수 */}
+    </View>
+  );
 }
 
 export default GameScreen;
