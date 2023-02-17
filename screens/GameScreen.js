a5c61a84ea6763a7a2027c52326e4bcb00cf28da
jangg0627 <jangg0627@naver.com>
2023-02-17 19:01:43 +0900

    ✨ 랜덤한 난수 생성 로직 구현 generateRandomBetween 함수를 사용해 난수 생성 가능 exclude를 받아 처음부터 게임이 끝나는 상황을 방지

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,16 @@
 import { Text, View, StyleSheet } from "react-native";
 import Title from "../components/Title";
 
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return exclude;
+  }
+}
+
 function GameScreen() {
   return (
     <View style={styles.screen}>
