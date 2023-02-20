7c67009933542c9916b0dbfadf4ee7fcdbb1b93e
jangg0627 <jangg0627@naver.com>
2023-02-20 16:02:43 +0900

    💄 게임오버 스크린 생성

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function GameOverScreen() {
+  return (
+    <View>
+      <Text>This GameOverScreen!</Text>
+    </View>
+  );
+}
+
+export default GameOverScreen;
