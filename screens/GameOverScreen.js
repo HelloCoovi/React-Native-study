8d07bfe7ba32a301035e7907f3b811dd8c663a6b
jangg0627 <jangg0627@naver.com>
2023-02-21 11:02:36 +0900

    💄 대략적인 레이아웃 생성

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,28 @@
 import { View, Text, StyleSheet } from "react-native";
 
+import Title from "../components/ui/Title";
+
 function GameOverScreen() {
   return (
-    <View>
-      <Text>This GameOverScreen!</Text>
+    <View style={styles.rootContainer}>
+      <Title>GameOver!</Title>
+      <View>
+        <Text>!IMAGE HERE!</Text>
+      </View>
+      <Text>
+        당신의 휴대폰은 숫자 X를 예측하기위해 Y 라운드가 필요했습니다.
+      </Text>
     </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
