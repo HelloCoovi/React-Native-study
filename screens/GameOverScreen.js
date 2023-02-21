3894b332994593412add8442030bde52b99e8c32
jangg0627 <jangg0627@naver.com>
2023-02-21 11:09:35 +0900

    🎯💄 이미지 추가

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
   return (
     <View style={styles.rootContainer}>
       <Title>GameOver!</Title>
-      <View>
-        <Text>!IMAGE HERE!</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
       </View>
       <Text>
         당신의 휴대폰은 숫자 X를 예측하기위해 Y 라운드가 필요했습니다.
@@ -24,5 +28,18 @@ const styles = StyleSheet.create({
     padding: 24,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    overflow: "hidden",
+    borderColor: Colors.primary800,
+    borderWidth: 3,
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
