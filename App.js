06132cf7f90bddbb7dd6eb936d459fb942a0e822
jangg0627 <jangg0627@naver.com>
2023-02-22 18:57:31 +0900

    ✨ categoriesScreen 로직 구현  - 요소는 Pressable로 해두었음  - FlatList의 renderItem을 함수화 해서 간략화

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import CategoriesScreen from "./screen/CategoriesScreen";
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <CategoriesScreen />;
 }
 
 const styles = StyleSheet.create({
