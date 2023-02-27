27f098205539d6a42f901e22c8d5e01d377d418d
jangg0627 <jangg0627@naver.com>
2023-02-27 14:31:02 +0900

    🎨 나눈 MealDetails로 코드 최적화

--- components/MealItem.js
+++ components/MealItem.js
@@ -7,6 +7,8 @@ import {
   Platform,
 } from "react-native";
 
+import MealDetails from "./MealDetails";
+
 import { useNavigation } from "@react-navigation/native";
 
 function MealItem({
@@ -33,11 +35,11 @@ function MealItem({
             <Image style={styles.image} source={{ uri: imageUrl }} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
         </View>
       </Pressable>
     </View>
@@ -72,16 +74,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     margin: 8,
-  },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
   },
   buttonPressed: {
     opacity: 0.85,
--- screen/MealDetailScreen.js
+++ screen/MealDetailScreen.js
@@ -1,14 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import MealDetails from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.id;
-  const mealTitle = route.params.title;
+  const selectMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View>
-      <Text>{mealId}</Text>
-      <Text>{mealTitle}</Text>
+      <Image style={styles.image} source={{ uri: selectMeal.imageUrl }} />
+      <Text>{selectMeal.title}</Text>
+      <MealDetails
+        duration={selectMeal.duration}
+        complexity={selectMeal.complexity}
+        affordability={selectMeal.affordability}
+      />
+      <Text>ingredients</Text>
+      {selectMeal.ingredients.map((ingredient, i) => {
+        return <Text key={i}>- {ingredient}</Text>;
+      })}
+      <Text>Step</Text>
+      {selectMeal.steps.map((step, i) => {
+        return (
+          <Text key={i}>
+            {i + 1}. {step}
+          </Text>
+        );
+      })}
     </View>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
