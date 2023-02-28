6cf1f6e5ae33cee83cffce9297a04d82d731a3b4
jangg0627 <jangg0627@naver.com>
2023-02-28 17:54:21 +0900

    🧠 Favorites에 속하는 데이터를 불러오는 로직 구현 & 불러오기

--- screen/FavoritesScreen.js
+++ screen/FavoritesScreen.js
@@ -1,7 +1,18 @@
 import { View, Text } from "react-native";
+import { useContext } from "react";
+
+import MealList from "../components/MealsList/MealList";
+
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  return <Text>This is FavoritesScreen!</Text>;
-}
+  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+  console.log(favoriteMeals);
 
+  return <MealList mealData={favoriteMeals} />;
+}
 export default FavoritesScreen;
