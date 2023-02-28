a7e961cf5a7590d8fa91f59e60c90a7d74152434
jangg0627 <jangg0627@naver.com>
2023-02-28 10:46:23 +0900

    🎯💄 Drawer를 스타일링하는 여러 옵션에 대해 배움  - options로 설정가능  - 배경,글자,문자, icon 등등 설정가능  - drawerIcon은 color와 size 변수를 지원하는데 아이콘의 색과 크기를 자동으로 어울리게 설정함

--- _Drawer-실습/App.js
+++ _Drawer-실습/App.js
@@ -3,15 +3,42 @@ import UserScreen from "./screens/UserScreen";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { DrawerActions } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="user" component={UserScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+        }}
+      >
+        <Drawer.Screen
+          name="welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome 🎉",
+            drawerActiveBackgroundColor: "#f0e1ff",
+            drawerActiveTintColor: "#3c0a6b",
+            drawerStyle: { backgroundColor: "#ccc" },
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="user"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
