2cfad24a9aaaa93ef497da1137d3b1d75eacc6db
jangg0627 <jangg0627@naver.com>
2023-02-28 17:17:41 +0900

    ♻️ component 나누기  - MealOverview에 있던 FlatList 코드를 재사용하기위해 component화 시킴

--- components/MealsList/MealList.js
+++ components/MealsList/MealList.js
@@ -1,5 +1,36 @@
-function MealList() {
-  return;
+import { View, FlatList, StyleSheet } from "react-native";
+
+import MealItem from "./MealItem";
+
+function MealList({ mealData }) {
+  function renderMealItem(itemData) {
+    const mealItemProps = {
+      id: itemData.item.id,
+      title: itemData.item.title,
+      imageUrl: itemData.item.imageUrl,
+      duration: itemData.item.duration,
+      complexity: itemData.item.complexity,
+      affordability: itemData.item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={mealData}
+        keyExtractor={(item) => item.id}
+        renderItem={(itemData) => renderMealItem(itemData)}
+      />
+    </View>
+  );
 }
 
 export default MealList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
--- screen/MealsOverviewScreen.js
+++ screen/MealsOverviewScreen.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import MealItem from "../components/MealsList/MealItem";
+import MealList from "../components/MealsList/MealList";
 
 import { MEALS, CATEGORIES } from "../data/dummy-data";
 
@@ -18,39 +19,11 @@ function MealsOverviewScreen({ route, navigation }) {
     });
   }, [catId, navigation]);
 
-  function renderMealItem(itemData) {
-    const mealItemProps = {
-      id: itemData.item.id,
-      title: itemData.item.title,
-      imageUrl: itemData.item.imageUrl,
-      duration: itemData.item.duration,
-      complexity: itemData.item.complexity,
-      affordability: itemData.item.affordability,
-    };
-    return <MealItem {...mealItemProps} />;
-  }
-
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={(itemData) => renderMealItem(itemData)}
-      />
-      <Text>Meals Overview Screen - {catId}</Text>
-    </View>
-  );
+  return <MealList mealData={displayedMeals} />;
 }
 
 export default MealsOverviewScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
