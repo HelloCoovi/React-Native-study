a726b175e1d923f08ce0283801e8b50ab55b2e8a
jangg0627 <jangg0627@naver.com>
2023-02-28 17:58:58 +0900

    ✨ Favorites가 없을때 출력 문구 지정

--- screen/FavoritesScreen.js
+++ screen/FavoritesScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useContext } from "react";
 
 import MealList from "../components/MealsList/MealList";
@@ -11,8 +11,29 @@ function FavoritesScreen() {
   const favoriteMeals = MEALS.filter((meal) =>
     favoriteMealsCtx.ids.includes(meal.id)
   );
-  console.log(favoriteMeals);
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
 
   return <MealList mealData={favoriteMeals} />;
 }
+
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
