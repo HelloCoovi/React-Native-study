9e11a06faa088006620a8ea4866a7e2328d61308
jangg0627 <jangg0627@naver.com>
2023-03-03 11:55:41 +0900

    ✨ 지출내역을 누르면 ManageExpense 화면으로 이동하게 구현  - useNavigation 사용

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -2,9 +2,13 @@ import { Pressable, View, Text, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
+import { useNavigation } from "@react-navigation/native";
+
 function ExpensesItem({ description, date, amount }) {
+  const navigation = useNavigation();
+
   function expensePressHandler() {
-    return;
+    navigation.navigate("ManageExpense");
   }
 
   return (
