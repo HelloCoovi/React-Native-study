cbb1211c3f7f5309b45530e20eec57321c3289ae
jangg0627 <jangg0627@naver.com>
2023-03-03 10:31:26 +0900

    🔨 더미데이터 생성

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,44 +1,45 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
   {
     id: "e1",
     description: "A pair of shoes",
     amount: 59.99,
-    data: new Date("2021-12-19"),
+    date: new Date("2021-12-19"),
   },
   {
     id: "e2",
     description: "A pair of trousers",
     amount: 89.99,
-    data: new Date("2022-01-05"),
+    date: new Date("2022-01-05"),
   },
   {
     id: "e3",
     description: "Some bananas",
     amount: 5.99,
-    data: new Date("2021-12-01"),
+    date: new Date("2021-12-01"),
   },
   {
     id: "e4",
     description: "A book",
     amount: 14.99,
-    data: new Date("2022-02-19"),
+    date: new Date("2022-02-19"),
   },
   {
     id: "e5",
     description: "Another book",
     amount: 18.59,
-    data: new Date("2022-02-18"),
+    date: new Date("2022-02-18"),
   },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
@@ -46,3 +47,11 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
