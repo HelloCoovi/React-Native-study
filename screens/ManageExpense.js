25b1baf9c6b49e0c2aacd4d4a95da03dc1b52bd2
jangg0627 <jangg0627@naver.com>
2023-03-05 04:26:35 +0900

    🎯✨ 버튼에 context 함수 적용  - 아직은 실제 데이터가 없기 때문에 각 버튼에 대응하는 더미 데이터를 직접넣음

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,10 +1,13 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
 import { GlobalStyles } from "../constants/styles";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
@@ -15,6 +18,7 @@ function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
 
@@ -23,6 +27,19 @@ function ManageExpense({ route, navigation }) {
   }
 
   function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "UPDATETest",
+        amount: 29.99,
+        date: new Date("2023-03-04"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "ADDTest",
+        amount: 19.99,
+        date: new Date("2023-03-05"),
+      });
+    }
     navigation.goBack();
   }
 
