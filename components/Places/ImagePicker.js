0e16fe4ea731d7e08e14bc9d37b26c8b0ba38096
jangg0627 <jangg0627@naver.com>
2023-03-07 16:13:05 +0900

    🎯✨ 사진 촬영기능 활성화  - launchCameraAsync 촬영기능, 속성으로 편집 여부 기타 등등 설정 가능  - 권한 Android는 권한 선택이 자동으로 열리지만 IOS는 설정해야함    -> 권한을 선택하지 않은경우, 권한 선택창 리턴    -> 거부할경우, 권한이 필요하다 알리고  false 리턴    -> 권한이 있는경우 true 리컨

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,5 +1,53 @@
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+
 function ImagePicker() {
-  return;
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "사진 사용 권한이 없습니다!",
+        "장소를 등록하기 위해서는 사진 권한을 허용 해야합니다."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
 }
 
 export default ImagePicker;
