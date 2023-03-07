d81a2b90340aa8a1cf490de7d219e5dc0fd7c2d7
jangg0627 <jangg0627@naver.com>
2023-03-07 11:50:45 +0900

    🔨 AllPlaces의 구성요소인 PlaceList 구현  - PlacesList 구현    -> fallback 텍스트 만들어둠    -> FlatList의 속성 지정, renderItem은 따로 component화  - props로 받은 데이터로 터치가 가능한 요소를 출력함

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,10 +1,35 @@
-import { FlatList, View } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
+import PlaceItem from "./PlaceItem";
 
-function PlacesList({places}){
-    return <View>
-        <FlatList data={places} keyExtractor={(item) => item.id} renderItem={}/>
+function PlacesList({ places }) {
+  if (!places || places.length === 0) {
+    return (
+      <View>
+        <Text>아직 추가하신 장소가 없네요 - 장소를 추가해보세요!</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={places}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PlaceItem place={item} />}
+      />
     </View>
-
+  );
 }
 
 export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
