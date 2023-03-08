aca227e52811f68c7382f9a5fd9f4d5083cb5bcd
jangg0627 <jangg0627@naver.com>
2023-03-08 17:24:44 +0900

    🎯✨ 지도를 클릭하면 마커가 생기는 기능 구현

--- screen/Map.js
+++ screen/Map.js
@@ -1,15 +1,40 @@
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
+import { useState } from "react";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="여기를 선택하셨어요!"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
