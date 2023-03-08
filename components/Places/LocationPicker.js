7bca98c9508ca9de1e3250a4b073e62aa69e7ec0
jangg0627 <jangg0627@naver.com>
2023-03-08 15:43:55 +0900

    🎯📦✨사용자 위치정보를 가져오는 기능 구현  - expo-location 패키지로 위치 정보 받아옴  - imagepicker 처럼 권한과 기능을 사용하기 때문에 두 코드는 비슷함  - 안드로이드는 location 객체가 잘 안불러와지는데 이것은 시뮬레이션에서만 그럼

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,24 +1,60 @@
-import { View, StyleSheet } from "react-native";
+import { Alert, View, StyleSheet } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
 
 function LocationPicker() {
-  function getLocationHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "위치정보 사용 권한이 없습니다!",
+        "장소를 등록하기 위해서는 위치 권한을 허용 해야합니다."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
 
   function pickOnMapHandler() {}
 
   return (
     <View>
-      <View style={styles.mapPreview}>
-        <View style={styles.actions}>
-          <OutlinedButton icon="location" onPress={getLocationHandler}>
-            Locate User
-          </OutlinedButton>
-          <OutlinedButton icon="map" onPress={pickOnMapHandler}>
-            Pick on Map
-          </OutlinedButton>
-        </View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
       </View>
     </View>
   );
